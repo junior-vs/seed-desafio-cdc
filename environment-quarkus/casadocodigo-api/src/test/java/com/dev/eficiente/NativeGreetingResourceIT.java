43e082f2211611c2b447572eeaed8125817e8792
Valdir Junior <junior_vs@outlook.com>
2021-03-07 18:46:30 -0300

    setup Projeto

---
+++ environment-quarkus/casadocodigo-api/src/test/java/com/dev/eficiente/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.dev.eficiente;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
