8369347d2dc6fc32a0d407f265dc5864191128f6
Valdir Junior <junior_vs@outlook.com>
2021-03-23 18:53:39 -0300

    limpeza

--- environment-quarkus/casadocodigo-api/src/main/java/com/dev/eficiente/casadocodigo/configs/MyObjectMapperCustomizer.java
+++ environment-quarkus/casadocodigo-api/src/main/java/com/dev/eficiente/casadocodigo/configs/MyObjectMapperCustomizer.java
@@ -1,4 +1,4 @@
-package com.dev.eficiente.resteasyjackson;
+package com.dev.eficiente.casadocodigo.configs;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- environment-quarkus/casadocodigo-api/src/test/java/com/dev/eficiente/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.dev.eficiente;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
